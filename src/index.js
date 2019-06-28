90090fb4c2f89d962c052633132433e6216172af
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2019-06-28 10:48:52 +0900

    CicleCI_08

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "./index.css";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
 ReactDOM.render(
   <div className="container">
     <App />
